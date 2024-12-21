8fbadfb3e095680ea80b2ce3a183342463a2f3f9
John-zzh <zehaozhoucase@gmail.com>
2024-12-21 15:40:37 +0800

    larget index at last

--- examples/large_fch/test_einsum.py
+++ examples/large_fch/test_einsum.py
@@ -1,6 +1,11 @@
 import time
 import numpy as np
 import gc
+
+a = np.arange(60).reshape(3,4,5)
+print(a)
+
+
 def use_einsum(eri3c, eri2c):
     start = time.time()
     result = np.einsum('pqP,PQ->pqQ',eri3c, eri2c)
@@ -18,21 +23,18 @@ def transpose_dot(eri3c, eri2c):
     print(f'transpose_dot: {time.time() - start:.2f} seconds')
 
 
-occ = 1600
-vir = 1600
-nauxbf = 200
-
-
-eri3c = np.random.rand(occ,vir,nauxbf)
-eri2c = np.random.rand(nauxbf,nauxbf)
-print('eri3c.flags \n',eri3c.flags)
-print('eri2c.flags \n',eri2c.flags)
-
-# use_einsum(eri3c, eri2c)
-normal_dot(eri3c, eri2c)
-
-gc.collect()
+occ = 180
+vir = 1800
+nauxbf = 280
 
 eri3c = np.random.rand(nauxbf, occ, vir)
 eri2c = np.random.rand(nauxbf,nauxbf)
 transpose_dot(eri3c, eri2c)
+
+
+eri3c = np.random.rand(occ,vir,nauxbf)
+eri2c = np.random.rand(nauxbf,nauxbf)
+normal_dot(eri3c, eri2c)
+
+
+
